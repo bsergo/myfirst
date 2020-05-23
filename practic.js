b5e0e35ed5cf0369021b5b0f236e35696f532c63
Sergey <serega.borovik@ukr.net>
2020-05-23 14:47:01 +0300

    Это наш первый комит

---
+++ practic.js
@@ -0,0 +1,8 @@
+let heigtC = 23;
+let width = 10;
+
+let s = heigtC * width;
+
+
+console.log(s)
+console.log(typeof(s))
---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict'
+/* 
+//Типы данных
+var number =5;                  // числовое
+var string = "Hello!";          // строка
+var sym =Symbol();              // символ
+var boolean = true;             // логическое
+null;                           // то чего не существует
+undefined;
+var obj ={}; 
+
+
+console.log(4/0);
+console.log('string'*9);
+*/
+
+//обьект
+// let persone ={
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+
+// };
+// console.log(persone.name);     // 1й способ вывести свойства обьекта
+// console.log(persone["name"]);  // 2й способ вывести свойства обьекта
+
+// массив
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[0]) // Номерация начинается с 0
+
+//alert("Hello world!")
+
+//let answer = confirm("Are you here?");
+//console.log (answer);
+
+// let answer = prompt("Есть ли вам 18??", "да");
+//console.log (typeof(arr));
+
+// console.log("arr" + " -object")
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+let isChecked = true;
+let isClose = false;
+console.log(isChecked && isClose); //щператор "И"
+console.log(isChecked || isClose) ; // оператор "или"
+
+
+
